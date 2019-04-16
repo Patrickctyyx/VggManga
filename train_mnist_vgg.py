49637c61f7018b14b8c94c8da8aeee5705316016
patrickcty <chengtiyanyang@gmail.com>
2019-04-17 00:25:18 +0800

    Update mnist model.

--- train_mnist_vgg.py
+++ train_mnist_vgg.py
@@ -11,30 +11,53 @@ import cv2
 
 
 def train_vgg_mnist():
-    config_str = 'configs/vgg_mnist_config.json'
-    config = process_config(config_str)
 
     print('[INFO] 加载数据…')
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     y_train = tf.keras.utils.to_categorical(y_train)
     y_test = tf.keras.utils.to_categorical(y_test)
 
-    x_train = np.resize(x_train, (x_train.shape[0], 48, 48, 3))
-    x_test = np.resize(x_test, (x_test.shape[0], 48, 48, 3))
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
 
-    x_train = x_train[: 10]
-    x_test = x_test[: 10]
-    y_train = y_train[: 10]
-    y_test = y_test[: 10]
+    # x_train = np.resize(x_train, (x_train.shape[0], 48, 48, 3))
+    # x_test = np.resize(x_test, (x_test.shape[0], 48, 48, 3))
 
-    # model = MangaFaceNetModel(config=config, use_vgg=True)
-    model = tf.keras.applications.vgg16.VGG16(weights=None, classes=10, input_shape=(48, 48, 3))
+    main_input = tf.keras.Input(shape=(28, 28, 1))
+
+    x = tf.keras.layers.Convolution2D(32, (3, 3), padding='same', name='conv1')(main_input)
+    x = tf.keras.layers.BatchNormalization(name='bn1')(x)
+    x = tf.keras.layers.ReLU()(x)
+    x = tf.keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same', name='pool1')(x)
+
+    x = tf.keras.layers.Convolution2D(48, (3, 3), padding='same', name='conv2')(x)
+    x = tf.keras.layers.BatchNormalization(name='bn2')(x)
+    x = tf.keras.layers.ReLU()(x)
+    x = tf.keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same', name='pool2')(x)
+
+    x = tf.keras.layers.Convolution2D(64, (3, 3), padding='same', name='conv3')(x)
+    x = tf.keras.layers.BatchNormalization(name='bn3')(x)
+    x = tf.keras.layers.ReLU()(x)
+    x = tf.keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same', name='pool3')(x)
+
+    x = tf.keras.layers.Flatten(name='fl')(x)
+
+    x = tf.keras.layers.Dense(3168, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(10, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=main_input, outputs=x)
+
     model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    print('[INFO] 使用 VGG 作为骨架')
 
     print('[INFO] 训练网络')
-    model.fit(x_train, y_train, epochs=5)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        'experiments',
+        verbose=1, save_weights_only=False,
+        monitor='val_loss',
+        mode='min',
+        save_best_only=True)
+    model.fit(x_train, y_train, epochs=10, callbacks=[cp_callback])
     print('[INFO] 训练完成…')
     model.evaluate(x_test, y_test)
 
